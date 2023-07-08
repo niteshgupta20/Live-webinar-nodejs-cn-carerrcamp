3218244115e5229fa4ff12cebf2a46def87c2672
niteshgupta20 <niteshguptampb19@gmail.com>
2023-07-08 23:07:51 +0530

    bug fix update student api:: require:mongoose

--- mern-app/server/routes/student/index.js
+++ mern-app/server/routes/student/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require("mongoose");
 const Candidate = require('../../models/candidate');
 
 router.get('/', async function callback(req, res) {
