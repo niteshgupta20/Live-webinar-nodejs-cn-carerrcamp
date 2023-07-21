3e4cdab32fae04d83fe40a3cdd05e46f0d0996d7
niteshgupta20 <niteshguptampb19@gmail.com>
2023-07-21 23:54:52 +0530

    added user in the request object

--- mern-app/server/middlewares/authMiddleware.js
+++ mern-app/server/middlewares/authMiddleware.js
@@ -6,21 +6,23 @@ exports.authMiddleware1 = async function (req, res, next) {
       const user = await Candidate.findOne({ email: req.cookies.user });
 
       if (user) {
+        // include user in the user object so that user is available in the API's
+        req.user = user
         next();
       } else {
-         return res.status(404).json({
-           message: "User Not Found"
-         });
+        return res.status(404).json({
+          message: 'User Not Found',
+        });
       }
     } else {
-       return res.status(403).json({
-         message: "Please Login Again"
-       });
+      return res.status(403).json({
+        message: 'Please Login Again',
+      });
     }
   } catch (error) {
-    console.log("**** AUTH MIDDLEWARE1 ****", error);
+    console.log('**** AUTH MIDDLEWARE1 ****', error);
     return res.status(500).json({
-      message: "Internal Server Error"
+      message: 'Internal Server Error',
     });
   }
-}
+};
