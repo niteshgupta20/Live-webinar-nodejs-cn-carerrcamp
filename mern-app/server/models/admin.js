da87b7c6fe47b766ceebe315a196a8668480c020
niteshgupta20 <niteshguptampb19@gmail.com>
2023-08-28 21:41:21 +0530

    admin signup

--- mern-app/server/models/admin.js
+++ mern-app/server/models/admin.js
@@ -1,22 +1,25 @@
 const mongoose = require('mongoose');
 
-const adminSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const adminSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
 const Admin = mongoose.model('Admin', adminSchema);
 module.exports = Admin;
--- mern-app/server/routes/admin/index.js
+++ mern-app/server/routes/admin/index.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Admin = require('../../models/admin');
 
+router.post('/', async function callback(req, res) {
+  try {
+    const isAdminExist = await Admin.findOne({ email: req.body.email });
+
+    if (!isAdminExist) {
+      const admin = await Admin.create(req.body);
+
+      console.log(admin);
+
+      return res.status(200).json({
+        message: 'Admin created successfully',
+        data: admin,
+      });
+    } else {
+      return res.status(200).json({
+        message: 'Admin Already Exists in database',
+      });
+    }
+  } catch (error) {
+    console.log('**** ADMIN SIGNUP API ****', error);
+    return res.status(500).json({
+      message: 'Internal Server Error',
+    });
+  }
+});
 
 module.exports = router;
